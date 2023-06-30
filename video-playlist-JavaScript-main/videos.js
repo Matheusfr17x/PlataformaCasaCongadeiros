f330eda11c4a3316541f21b44bb3e324b0884a1d
Matheusfr17x <61791461+Matheusfr17x@users.noreply.github.com>
2023-06-30 18:18:02 -0300

    Add files via upload

---
+++ video-playlist-JavaScript-main/videos.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    src: "./img/01.mp4",
+    title: "Editando documentos",
+  },
+  {
+    src: "./img/02.mp4",
+    title: "editando planilhas",
+  },
+  {
+    src: "./img/03.mp4",
+    title: "criando imagens",
+  },
+  {
+    src: "./img/04.mp4",
+    title: "introdução a sistemas",
+  },
+  {
+    src: "./img/05.mp4",
+    title: "Likes",
+  },
+  {
+    src: "./img/06.mp4",
+    title: "testando word",
+  },
+  {
+    src: "./img/07.mp4",
+    title: "Criando PDF",
+  },
+];
